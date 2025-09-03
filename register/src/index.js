7617eb887b60e4822a312b0747d107c3642f6d00
Amol Anand <amol.anand@gmail.com>
2025-09-02 21:32:33 -0400

    updating register endpoint

--- register/src/index.js
+++ register/src/index.js
@@ -19,7 +19,14 @@
  */
 async function registerRequest(env, org, site) {
   try {
+    // first check if the folder already exists
+    const folder = await env.R2_BUCKET.get(`registered/${org}--${site}`);
+    if (folder) {
+      console.debug('Register Request: ', org, site, 'Folder already exists');
+      return 'Folder already exists';
+    }
     await env.R2_BUCKET.put(`registered/${org}--${site}`, `{ "org": "${org}", "site": "${site}" }`);
+    return 'Folder created';
   } catch (err) {
     console.error('Register Request failed: ', org, site, err);
     throw err;
@@ -28,27 +35,37 @@ async function registerRequest(env, org, site) {
 
 export default {
   async fetch(request, env) {
-    // register the incoming request by creating a folder in the R2 bucket
-    // Only allow posts to the /register endpoint
-    if (request.url !== '/register') {
-      return new Response('Not found', { status: 404 });
-    }
-    if (request.method !== 'POST') {
-      return new Response('Method not allowed', { status: 405 });
-    }
-    if (!request.body) {
-      return new Response('No body', { status: 400 });
-    }
-    const { org, site } = request.body;
-    if (!org || !site) {
-      return new Response('Invalid body. Please provide org and site', { status: 400 });
-    }
     try {
-      await registerRequest(env, org, site);
+      const requestUrl = new URL(request.url);
+      // register the incoming request by creating a folder in the R2 bucket
+      // Only allow posts to the /register endpoint
+      if (requestUrl.pathname !== '/register') {
+        return new Response('Not found', { status: 404 });
+      }
+      if (request.method !== 'POST') {
+        return new Response('Method not allowed', { status: 405 });
+      }
+      if (!request.body) {
+        return new Response('No body', { status: 400 });
+      }
+      const data = await request.json();
+      const { org, site } = data;
+      if (!org || !site) {
+        return new Response('Invalid body. Please provide org and site', { status: 400 });
+      }
+      try {
+        const result = await registerRequest(env, org, site);
+        if (result === 'Folder already exists') {
+          return new Response('This org/site has already been registered', { status: 200 });
+        }
+        return new Response('Registration successful!', { status: 200 });
+      } catch (err) {
+        console.error('Registration failed: ', org, site, err);
+        return new Response(`Registration failed: ${err.message}`, { status: 500 });
+      }
     } catch (err) {
-      console.error('Registration failed: ', org, site, err);
+      console.error('Registration failed: ', request.url, err);
       return new Response(`Registration failed: ${err.message}`, { status: 500 });
     }
-    return new Response('Registration successful!', { status: 200 });
   },
 };
