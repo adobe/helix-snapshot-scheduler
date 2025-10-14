684e71ed702186a6b6d3ebe1e9294207fa7c000b
Amol Anand <amol.anand@gmail.com>
2025-10-14 18:06:11 -0400

    fix: allowing for da-nx testing

--- register/src/index.js
+++ register/src/index.js
@@ -26,7 +26,7 @@ function getAllowedOrigins() {
 
   // Only allow localhost in CI environment for development/testing
   if (globalEnv?.ENVIRONMENT === 'ci') {
-    return [...baseOrigins, 'http://localhost:3000'];
+    return [...baseOrigins, 'http://localhost:3000', 'http://localhost:6456'];
   }
 
   return baseOrigins;
